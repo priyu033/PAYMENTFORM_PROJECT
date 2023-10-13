f7fe740896973d6b3d6bdf67fe0366b72b6cfb09
priyu033 <112699288+priyu033@users.noreply.github.com>
2023-10-14 00:31:16 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+form {
+    width: 500px;
+    margin: 0 auto;
+  }
+  
+  input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  button {
+    background-color: #000;
+    color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+  }
+  
